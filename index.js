b8f1e2624363ad198a681125351f52def4921b98
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-10-28 14:35:29 +0700

    Test database connection and sync models

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Test database connection and sync models
+app.listen(PORT, async () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+})
+    .catch((err) => {
+        console.log(err);
+})
 
