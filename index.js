b09b5da89be77ecbffc2b8e3075370dae823a5a5
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-10-28 14:57:09 +0700

    CRUD Endpoints for Komik model

--- index.js
+++ index.js
@@ -20,3 +20,52 @@ db.sequelize.sync().then((result) => {
         console.log(err);
 })
 
+// CRUD Endpoints for Komik model
+// 
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (error) {
+        res.status(500).send({ messafe: error.message });
+    }
+});
+
+app.get('/komik', async (req, res) => {
+    try {
+        const komik = await db.Komik.findAll();  
+        res.send(komik);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }   
+});
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.update(data);
+        res.send({ message: 'Komik updated successfully', komik });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.destroy();
+        res.send({ message: 'Komik deleted successfully' });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
